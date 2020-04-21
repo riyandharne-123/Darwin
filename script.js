54755b2991a1b562e0a6af47041d3a6dd9fe7bae
riyandharne-123 <60026968+riyandharne-123@users.noreply.github.com>
2020-04-21 19:53:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+//back to top button
+//scroll down function
+function topFunction(){
+  //  document.body.scrollTop = 0; // For Safari
+    //document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    $('html, body').animate({scrollTop:0}, '500');  
+} 
